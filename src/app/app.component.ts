34d09df86542d3dede6a201c9c55cd82f49b9545
pim-t <piraya.qt@gmail.com>
2021-04-18 18:54:22 +1000

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import {webSocket, WebSocketSubject} from 'rxjs/webSocket';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  myWebSocket: WebSocketSubject<any> = webSocket('ws://localhost:7000');
+
+  constructor() {
+    this.myWebSocket.subscribe(
+      msg => console.log('message received: ' + msg),
+      // Called whenever there is a message from the server
+      err => console.log(err),
+      // Called if WebSocket API signals some kind of error
+      () => console.log('complete')
+      // Called when connection is closed (for whatever reason)
+   );
+  }
+
+  sendMessageToServer() {
+    console.log("Clicked")
+    this.myWebSocket.next({message: 'some message'});
+  }
+
+}
